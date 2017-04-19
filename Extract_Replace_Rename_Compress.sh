71717ea4c29708c21cb51d734fdcd5a0d606847d
Autogal <nagaradhika.chunduri@gmail.com>
2017-04-19 18:58:04 -0400

    Update Extract_Replace_Rename_Compress.sh
    Working Copy

--- Extract_Replace_Rename_Compress.sh
+++ Extract_Replace_Rename_Compress.sh
@@ -1,3 +1,4 @@
+#!/bin/sh 
 input=$1
 echo $input
 vLen=`echo ${#input}`
@@ -6,14 +7,11 @@ tar -zxvf cmdm_IH_csvs_218132.tar.gz --directory \Extracted_Source; echo "extrac
 output=${input:0:${#input}-1}
 echo ${#output}
 echo $output
-#Leave the echo end in the below command to avoid adding a new line character at the end of the file.
+#Leave the echo end in the below command to avoid adding a new line character at the end of the file. $(date "+%Y.%m.%d-%H.%M.%S")
 #for a in `cat native_location_na_ih_BASE.csv`; do echo ${a//"218971"/${output:0:${#output}-1}}; done > native_location_na_ih_${output:0:${#output}-1}.csv; echo end
-#!/bin/sh 
 find ./Extracted_Source -type f | xargs sed -i -e "s/218132/${output:0:${#output}-1}/" {} \;
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+current_time=$(date "+%Y%m%dT%H%M%S")
 echo "Current Time : $current_time"
-#for f in ./Extracted_Source/*; do mv -- "$f" "$f_${current_time:0:${#current_time}-1}";
-for f in * ; do echo "${f%.*}-${current_time:0:${#current_time}-1}.${f##*.}"; done;
-#find ./Extracted_Source -exec -e touch -t ${current_time:0:${#current_time}-1} {} \;
-tar -czvf cmdm_IH_csvs_${output:0:${#output}-1}_${current_time:0:${#current_time}-1}.tar.gz Extracted_Source;
-#./Extracted_Source/*
+find ./Extracted_Source -type f -name "*.csv" | while read -r files; do mv "$files" "${files%.*}_${current_time:0:${#current_time}-1}.csv"; done;
+echo "After find csv files and rename with timestamp"
+tar -czvf cmdm_IH_csvs_${output:0:${#output}-1}_${current_time:0:${#current_time}-1}.tar.gz Extracted_Source; echo "End of the whole process";
